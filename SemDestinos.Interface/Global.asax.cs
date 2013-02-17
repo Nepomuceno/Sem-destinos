271b3e45929e63440bf06acad628b57a691e29e9
Gabriel <gabriel.bico@gmail.com>
2013-02-17 09:14:14 -0300

    Removing webapi for complete

--- SemDestinos.Interface/Global.asax.cs
+++ SemDestinos.Interface/Global.asax.cs
@@ -16,7 +16,6 @@ namespace SemDestinos.Interface
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
